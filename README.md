7a95ea144e390e567ddde5cc4a19f7b0b4282117
Alexande A. Kuzkin <xbevice@gmail.com>
2019-02-09 16:29:32 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,21 @@
+# Eagle cad module copier
+
+How to use:
+1. Create schematic with modules (e.g.: MODULE1, MODULE2, MODULE3 etc)
+2. Create the board and route MODULE1 layout
+4. run:
+   
+    python module_copy.py --in board.brd --out copied.brd \
+    --ref-design MODULE1 \
+    --ref-element R1 \
+    --modify-module MODULE2 MODULE3 MODULE4
+    
+Where: 
+
+* *--in* - input file
+* *--out* - output file
+* *--ref-design* - module whose design will be used as the reference
+* *--ref-element* - element name whose place will be used as the reference
+* *--modify-module* - names of modules whose layouts will be modified
+
+
--- module_copy.py
+++ module_copy.py
@@ -1,14 +1,24 @@
 from xml.etree.ElementTree import Element, parse
 from copy import deepcopy
 from decimal import Decimal
+import argparse
 
-filename = "c:/Users/bevice/eagle/projects/bulychev/pwr/pwr_profile_test1.brd"
-f2 = "c:/Users/bevice/eagle/projects/bulychev/pwr/pwr_profile_test2.brd"
 
-REF_DESIGN = "CHARGER1"
-MODIFY_DESIGN = ["CHARGER2", "CHARGER3", "CHARGER4", "CHARGER5"]
-REF_ELEMENT = "IC2"
 
+
+parser = argparse.ArgumentParser(description="Copy EAGLE module layout")
+parser.add_argument('--in', action='store', dest='IN_FILE', help='Input file', type=str, required=True)
+parser.add_argument('--out', action='store', dest='OUT_FILE', help='Output file', type=str, required=True)
+parser.add_argument('--ref-design', action='store', dest='REF_DESIGN', required=True, type=str,
+                    help='Module name with reference design')
+parser.add_argument('--ref-element', action='store', dest='REF_ELEMENT', required=True, type=str,
+                    help='Element name (without module prefix), whose place will be used as the reference coords')
+parser.add_argument('--modify-module', action='store', dest='MODIFY_DESIGN', nargs="+", required=True, type=str,
+                    help='Name of module whose layouts will be modified')
+
+
+
+args = parser.parse_args()
 
 def get_module_name(element_name):
     if not ":" in element_name:
@@ -54,7 +64,7 @@ class Signal:
         self.ref_coords(-new_origin_x, -new_origin_y)
 
 
-tree = parse(filename)
+tree = parse(args.IN_FILE)
 root = tree.getroot()
 
 positions = {}
@@ -64,7 +74,7 @@ modify_zeroes = {}
 
 for e in root.iter("element"):
     name = get_element_name(e.attrib["name"])
-    if get_module_name(e.attrib["name"]) == REF_DESIGN:
+    if get_module_name(e.attrib["name"]) == args.REF_DESIGN:
         attribs = {}
         positions[name] = {"x": Decimal(e.attrib["x"]), "y": Decimal(e.attrib["y"]),
                            "attribs": attribs}
@@ -74,10 +84,10 @@ for e in root.iter("element"):
         for a in e.iter("attribute"):
             if len({"name", "x", "y"} & set(a.attrib.keys())) == 3:
                 attribs[a.attrib["name"]] = {"x": Decimal(a.attrib["x"]), "y": Decimal(a.attrib["y"])}
-        if name == REF_ELEMENT:
+        if name == args.REF_ELEMENT:
             ref_position = {"x": Decimal(e.attrib["x"]), "y": Decimal(e.attrib["y"])}
-    if get_module_name(e.attrib["name"]) in MODIFY_DESIGN:
-        if name == REF_ELEMENT:
+    if get_module_name(e.attrib["name"]) in args.MODIFY_DESIGN:
+        if name == args.REF_ELEMENT:
             modify_zeroes[get_module_name(e.attrib["name"])] = {"x": Decimal(e.attrib["x"]),
                                                                 "y": Decimal(e.attrib["y"])}
 
@@ -92,7 +102,7 @@ if ref_position is not None:
 
 # Изменяем координаты
 for e in root.iter("element"):
-    if get_module_name(e.attrib["name"]) in MODIFY_DESIGN:
+    if get_module_name(e.attrib["name"]) in args.MODIFY_DESIGN:
         name = get_element_name(e.attrib["name"])
         module = get_module_name(e.attrib["name"])
         e.attrib["x"] = str(modify_zeroes[module]["x"] + positions[name]["x"])
@@ -107,14 +117,14 @@ for e in root.iter("element"):
 
 ref_signals = {}
 for signal in root.iter("signal"):
-    if get_module_name(signal.attrib["name"]) == REF_DESIGN:
+    if get_module_name(signal.attrib["name"]) == args.REF_DESIGN:
         le = Signal(deepcopy(signal))
         ref_signals[get_element_name(signal.attrib["name"])] = le
         le.ref_coords(ref_position["x"], ref_position["y"])
         le.element.attrib["name"] = get_element_name(le.element.attrib["name"])
 
 for signal in root.iter("signal"):
-    if get_module_name(signal.attrib["name"]) not in MODIFY_DESIGN:
+    if get_module_name(signal.attrib["name"]) not in args.MODIFY_DESIGN:
         continue
     signal_name = get_element_name(signal.attrib["name"])
     module_name = get_module_name(signal.attrib["name"])
@@ -133,6 +143,6 @@ for signal in root.iter("signal"):
         for element in le.element.iter("polygon"): signal.append(element)
         for element in le.element.iter("via"): signal.append(element)
 
-tree.write(open(f2, "wb"))
+tree.write(open(args.OUT_FILE, "wb"))
 
 print("Done")
